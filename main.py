c5efee87fc45b4a1b323c9f6e137cebed156856d
Odaviespecifico <jak_singer@hotmail.com>
2025-05-30 11:31:28 -0300

    Fixed some bugs in the restarting of the game

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from menu import tela_inicial
 
 
 if __name__ == '__main__':
-    g = Game()
     while True:
+        g = Game()
         tela_inicial()
         g.playing = True
         g.game_loop()
